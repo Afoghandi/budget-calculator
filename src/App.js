41bae35c0677d47a4c586d4cbd009996aa68451b
Afo Oshun <afoyenyen@Afos-iMac.local>
2020-05-22 15:13:23 +0100

    setting up expense list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+//import logo from './logo.svg';
+import "./App.css";
+import Alert from "./components/Alert";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
+import uuid from "uuid/dist/v4";
+
+const initialExpense = [
+	{ id: uuid(), charge: "rent", amount: 1600 },
+	{ id: uuid(), charge: "card payment", amount: 100 },
+	{ id: uuid(), charge: "card removealt", amount: 2600 },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	//****state values******* */
+
+	// all expenses , add expense
+	const [expenses, setExpenses] = useState(initialExpense);
+	// single expense
+	const [charge, setCharge] = useState("");
+
+	//single amount
+	const [amount, setAmount] = useState("");
+	//****  functionality   ******* */
+
+	const handleCharge = (e) => {
+		setCharge(e.target.value);
+	};
+	const handleAmount = (e) => {
+		setAmount(e.target.value);
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (charge !== "" && amount > 0) {
+			const singleExpense = { id: uuid(), charge, amount };
+			setExpenses([...expenses, singleExpense]);
+			setCharge("");
+			setAmount("");
+		} else {
+			//handle alert
+		}
+	};
+	return (
+		<>
+			<Alert />
+			<h1>budget calculator</h1>
+			<main className="App">
+				<ExpenseForm
+					charge={charge}
+					amount={amount}
+					handleCharge={handleCharge}
+					handleAmount={handleAmount}
+					handleSubmit={handleSubmit}
+				/>
+				<ExpenseList expenses={expenses} />
+			</main>
+			<h1>
+				total spending:
+				<span className="total">
+					$
+					{expenses.reduce((acc, curr) => {
+						return (acc += parseFloat(curr.amount));
+					}, 0)}
+				</span>{" "}
+			</h1>
+		</>
+	);
 }
 
 export default App;
